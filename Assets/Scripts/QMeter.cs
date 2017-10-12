35980a6187bd1a43b1bd68cf246046320d490279
LAPTOP-KN03C474\tamin <jckcrrgn@gmail.com>
2017-10-12 17:17:40 -0600

    Changed QMeter's switch statements to if-else to accommodate non-divisible-by-10 PTP system.

--- Assets/Scripts/QMeter.cs
+++ Assets/Scripts/QMeter.cs
@@ -152,79 +152,85 @@ public class QMeter : MonoBehaviour {
 			Qgrade=Qmax;
 		}
 
-		switch (Qgrade) {
-		case 100:
-			agua.GetComponent<QspriteSwitch>().setSprite (0);
-			break;
-		case 90:
-			agua.GetComponent<QspriteSwitch>().setSprite (1);
-			break;
-		case 80:
-			agua.GetComponent<QspriteSwitch>().setSprite (2);
-			break;
-		case 70:
-			agua.GetComponent<QspriteSwitch>().setSprite (3);
-			break;
-		case 60:
-			agua.GetComponent<QspriteSwitch>().setSprite (4);
-			break;
-		default:
-			Debug.Log("Failure of QMeter's switch statement");
-			break;
-		}
+        if (Qgrade > 90)
+        {
+            agua.GetComponent<QspriteSwitch>().setSprite(0);
+        }
+        else if (Qgrade > 80)
+        {
+            agua.GetComponent<QspriteSwitch>().setSprite(1);
+        }
+        else if (Qgrade > 70)
+        {
+            agua.GetComponent<QspriteSwitch>().setSprite(2);
+        }
+        else if (Qgrade > 60)
+        {
+            agua.GetComponent<QspriteSwitch>().setSprite(3);
+        }
+        else if (Qgrade >= 50)
+        {
+            agua.GetComponent<QspriteSwitch>().setSprite(4);
+        }
+        else
+        {
+            Debug.Log("Failure of QMeter's switch statement");
+        }
 
-        
 
-		//agua.GetComponent<QspriteSwitch>().previousSprite ();
 
-//		//down the R, up the B of Q's color RGB
-//		currentQ.color = new Color (QcolorR-deltaQ, QcolorG, QcolorB+deltaQ, QcolorA);
-//
-//		//prevent R from going negative 
-//		if (currentQ.color.r < 0f) {
-//			Color temp = currentQ.color;
-//			temp.r=0f;
-//			currentQ.color=temp;
-//		}
-//
-//		//prevent the B from exceeding 1
-//		if(currentQ.color.b>1f){
-//			Color temp = currentQ.color;
-//			temp.b=1f;
-//			currentQ.color=temp;
-//		}
-//		
-//		QcolorR = currentQ.color.r;
-//		QcolorB = currentQ.color.b;
-//		QcolorG = currentQ.color.g;
-//		QcolorA = currentQ.color.a;
-//
-//		Debug.Log ("Q Grade = "+Qgrade);
-//
-	}
+        //agua.GetComponent<QspriteSwitch>().previousSprite ();
+
+        //		//down the R, up the B of Q's color RGB
+        //		currentQ.color = new Color (QcolorR-deltaQ, QcolorG, QcolorB+deltaQ, QcolorA);
+        //
+        //		//prevent R from going negative 
+        //		if (currentQ.color.r < 0f) {
+        //			Color temp = currentQ.color;
+        //			temp.r=0f;
+        //			currentQ.color=temp;
+        //		}
+        //
+        //		//prevent the B from exceeding 1
+        //		if(currentQ.color.b>1f){
+        //			Color temp = currentQ.color;
+        //			temp.b=1f;
+        //			currentQ.color=temp;
+        //		}
+        //		
+        //		QcolorR = currentQ.color.r;
+        //		QcolorB = currentQ.color.b;
+        //		QcolorG = currentQ.color.g;
+        //		QcolorA = currentQ.color.a;
+        //
+        //		Debug.Log ("Q Grade = "+Qgrade);
+        //
+    }
 
     public void setWaterSprite()
     {
-        switch (Qgrade)
+        if (Qgrade > 90)
         {
-            case 100:
-                agua.GetComponent<QspriteSwitch>().setSprite(0);
-                break;
-            case 90:
-                agua.GetComponent<QspriteSwitch>().setSprite(1);
-                break;
-            case 80:
-                agua.GetComponent<QspriteSwitch>().setSprite(2);
-                break;
-            case 70:
-                agua.GetComponent<QspriteSwitch>().setSprite(3);
-                break;
-            case 60:
-                agua.GetComponent<QspriteSwitch>().setSprite(4);
-                break;
-            default:
-                Debug.Log("Failure of QMeter's switch statement");
-                break;
+            agua.GetComponent<QspriteSwitch>().setSprite(0);
+        }else if (Qgrade > 80)
+        {
+            agua.GetComponent<QspriteSwitch>().setSprite(1);
+        }
+        else if (Qgrade > 70)
+        {
+            agua.GetComponent<QspriteSwitch>().setSprite(2);
+        }
+        else if (Qgrade > 60)
+        {
+            agua.GetComponent<QspriteSwitch>().setSprite(3);
+        }
+        else if (Qgrade >= 50)
+        {
+            agua.GetComponent<QspriteSwitch>().setSprite(4);
+        }
+        else
+        {
+            Debug.Log("Failure of QMeter's switch statement");
         }
     }
 }
